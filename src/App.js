41466070bf0d615ebd0dc45dc5a7878810faa2c6
optimajet <support@optimajet.com>
2022-05-31 17:36:17 +0200

    The version 5.3.5 (autocommit)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import WorkflowDesigner from '@optimajet/workflow-designer-react';
 
 function App() {
   // ---------- Parameters for Workflow Designer ---------------
-  const schemeCode = "SimpleWF";
+  const schemeCode = "WfeTest";
   const processId = "204B2876-D749-4743-9D44-BA27BDBECC12";
   const designerConfig = {
     renderTo: 'wfdesigner',
-    apiurl: 'https://workflowengine.io/demo/Designer/API',
+    apiurl: 'http://localhost:5000/Designer/API',
     templatefolder: './templates/',
     widthDiff: 0,
     heightDiff: 0
