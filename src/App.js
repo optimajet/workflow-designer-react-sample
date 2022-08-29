627b50b0ffdf02a94f7514c9792e27df1337b824
optimajet <support@optimajet.com>
2022-08-29 19:27:28 +0200

    The version 7.0.0 (autocommit)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import './App.css'
 import WorkflowDesigner from '@optimajet/workflow-designer-react';
+import el from './localization/el.json';
+import elElementUILocalization from './localization/el.js';
 
+el.elementUILocalization = elElementUILocalization;
 
-function App() {
-  // ---------- Parameters for Workflow Designer ---------------
-  const schemeCode = "WfeTest";
-  const processId = "204B2876-D749-4743-9D44-BA27BDBECC12";
-  const designerConfig = {
-    renderTo: 'wfdesigner',
-    apiurl: 'http://localhost:5000/Designer/API',
-    templatefolder: './templates/',
-    widthDiff: 0,
-    heightDiff: 0
-  };
-
-  return (
-    <WorkflowDesigner
-      schemeCode={schemeCode}
-      processId={processId}
-      designerConfig={designerConfig}
-    />
-  );
+// ---------- Parameters for Workflow Designer ---------------
+const props = {
+  designerConfig: {
+    apiurl: 'https://workflowengine.io/demo/Designer/API',
+    name: 'wfe',
+    language: 'en',
+    customLocalization: [el]
+  },
+  schemeCode: 'SimpleWF',
+  loadError: error => console.error(error),
+  onLoadDesigner: () => {
+  },
+  readOnly: false,
 }
+
+const App = () => <WorkflowDesigner {...props} />
 
 export default App;
